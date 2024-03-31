d39475d816b3ae9a9a219439458e8895aaaf88a7
Pat <patrycjatr2001@gmail.com>
2024-03-31 21:30:29 +0200

    Entity, Controller and Script

--- zad02/src/Controller/ProductController.php
+++ zad02/src/Controller/ProductController.php
@@ -2,56 +2,84 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
+    
 class ProductController extends AbstractController
 {
-    private $products = ["prod_1", "prod_2", "prod_3", "prod_4"];
-
     #[Route('/products', name: 'app_products', methods: ['GET'])]
-    public function getProducts(): JsonResponse
+    public function getProducts(ProductRepository $productRepository): JsonResponse
     {
-        return $this->json($this->products);
-    }
+        $products = $productRepository->findAll();
+        return $this->json($products);
+    } 
 
     #[Route('/product/{id}', name: 'app_product_show', methods: ['GET'])]
-    public function getProduct($id): JsonResponse
+    public function getProduct(ProductRepository $productRepository, $id, PersistenceManagerRegistry $doctrine): JsonResponse
     {
-        if ($id < 0 || $id >= count($this->products)) {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
             return $this->json(['error' => 'Product not found'], 404);
         }
-        return $this->json($this->products[$id]);
+
+        return $this->json($product);
     }
 
     #[Route('/product', name: 'app_product_create', methods: ['POST'])]
-    public function addProduct(Request $request): JsonResponse
+    public function addProduct(Request $request, PersistenceManagerRegistry $doctrine): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-        $this->products[] = $data['name'];
-        return $this->json($this->products[count($this->products) - 1], 201);
+
+        $entityManager = $doctrine->getManager();
+        $product = new Product();
+        $product->setName($data['name']);
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json($product, 201);
     }
 
     #[Route('/product/{id}', name: 'app_product_update', methods: ['PUT'])]
-    public function updateProduct($id, Request $request): JsonResponse
+    public function updateProduct(ProductRepository $productRepository, $id, Request $request, PersistenceManagerRegistry $doctrine): JsonResponse
     {
-        if ($id < 0 || $id >= count($this->products)) {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
             return $this->json(['error' => 'Product not found'], 404);
         }
+
         $data = json_decode($request->getContent(), true);
-        $this->products[$id] = $data['name'];
-        return $this->json($this->products[$id]);
+        $product->setName($data['name']);
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json($product);
     }
 
     #[Route('/product/{id}', name: 'app_product_delete', methods: ['DELETE'])]
-    public function deleteProduct($id): JsonResponse
+    public function deleteProduct(ProductRepository $productRepository, $id, PersistenceManagerRegistry $doctrine): JsonResponse
     {
-        if ($id < 0 || $id >= count($this->products)) {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
             return $this->json(['error' => 'Product not found'], 404);
         }
-        unset($this->products[$id]);
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($product);
+        $entityManager->flush();
+
         return $this->json(null, 204);
     }
 }
+
